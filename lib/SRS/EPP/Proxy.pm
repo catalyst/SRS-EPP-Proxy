ef9eda53197288958cc29a1d5abd3f63aa74c066
Sam Vilain <sam.vilain@catalyst.net.nz>
2009-06-26 04:02:19 +1200

    Add module stub
    Without modules to test, Test::Builder complains that no tests were planned
    and the tests fail.
    
    Signed-off-by: Sam Vilain <sam.vilain@catalyst.net.nz>

---
+++ lib/SRS/EPP/Proxy.pm
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2009  NZ Registry Services
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Artistic License 2.0 or later.  You should
+# have received a copy of the Artistic License the file COPYING.txt.
+# If not, see <http://www.perlfoundation.org/artistic_license_2_0>
+
+package SRS::EPP::Proxy;
+
+our $VERSION = "0.00_01";
+
+1;
+
+__END__
+
+=head1 NAME
+
+SRS::EPP::Proxy - IETF EPP <=> SRS XML proxy software
+
+=head1 SYNOPSIS
+
+ ...
+
+=head1 DESCRIPTION
+
+This module is the centre point for the various modules which drive
+the SRS / EPP proxy.  For now, it is just a stub.
+
+=cut
+
