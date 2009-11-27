20cd953910b4677b200ee982849f7d4b693e9e3b
Sam Vilain <sam.vilain@catalyst.net.nz>
2009-11-27 13:46:29 +1300

    wr65223 - move SRS::EPP::Message::EPP to XML::EPP
    It makes no sense really for the EPP-related messages to be buried inside
    the SRS::EPP namespace.  Move them out to XML::EPP (and some other
    re-organizations).
    
    We will eventually cut PRANG and XML::EPP as independent CPAN releases.

---
+++ lib/PRANG/Graph/Class.pm
@@ -0,0 +1,13 @@
+
+package PRANG::Graph::Class;
+
+# this is a role which must be mixed into classes which have
+# PRANG::Graph::Meta::Attr and PRANG::Graph::Meta::Element attributes
+
+use Moose::Role;
+
+requires 'xmlns';
+requires 'attributes';
+requires 'elements';
+
+1;
