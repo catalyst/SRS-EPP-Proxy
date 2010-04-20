f4aac0bc2709bbc1647cd78f079cc38c41d288bb
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-04-21 11:17:03 +1200

    Add tests, docs and bugfixes for the Backend Queue

--- lib/SRS/EPP/Session/BackendQ.pm
+++ lib/SRS/EPP/Session/BackendQ.pm
@@ -1,16 +1,16 @@
 
 package SRS::EPP::Session::BackendQ;
 
-use SRS::EPP::Command;
 use SRS::Request;
 use SRS::Response;
+use SRS::EPP::Command;
 
 use Moose;
 use MooseX::Method::Signatures;
 
 has 'queue' =>
 	is => "ro",
-	isa => "ArrayRef[SRS::Request]",
+	isa => "ArrayRef[ArrayRef[SRS::Request]]",
 	default => sub { [] },
 	;
 
@@ -22,8 +22,8 @@ has 'owner' =>
 
 has 'responses' =>
 	is => "ro",
-	isa => "HashRef[SRS::Response]",
-	default => sub { {} },
+	isa => "ArrayRef[ArrayRef[SRS::Response]]",
+	default => sub { [] },
 	;
 
 has 'sent' =>
@@ -39,59 +39,76 @@ has 'session' =>
 
 # add a response corresponding to a request
 method queue_backend_request( SRS::EPP::Command $cmd, SRS::Request @rq ) {
-	for my $rq ( @rq ) {
-		push @{ $self->queue }, $rq;
-		push @{ $self->owner }, $cmd
-	}
+	push @{ $self->queue }, \@rq;
+	push @{ $self->responses }, [];
+	push @{ $self->owner }, $cmd;
+}
+
+use List::Util qw(sum);
+
+method queue_size() {
+	sum 0, map { scalar @$_ } @{$self->queue};
+}
+method queue_flat() {
+	map { @$_ } @{$self->queue};
 }
 
 # get the next N backend messages to be sent; marks them as sent
 method backend_next( Int $how_many = 1 ) {
+	return unless $how_many;
 	my $sent = $self->sent;
-	my $waiting = @{ $self->queue } - $sent;
-	my @rv = @{ $self->queue }[ $sent .. $sent + $how_many ];
+	my $waiting = $self->queue_size - $sent;
+	$how_many = $waiting if $how_many > $waiting;
+	my @rv = ($self->queue_flat)[ $sent .. $sent + $how_many - 1 ];
 	$self->sent($sent + @rv);
 	return @rv;
 }
 
 method backend_pending() {
 	my $sent = $self->sent;
-	my $waiting = @{ $self->queue } - $sent;
+	my $waiting = $self->queue_size - $sent;
 	return $waiting;
 }
 
-# add a response corresponding to a request
-method add_backend_response(
-	SRS::ActionID $id, SRS::Response $response
-       )
+# add a response corresponding to a request - must be in order as
+# there is no other way to correlate read-only responses with their
+# requests (no client_tx_id in SRS requests)
+method add_backend_response( SRS::Request $request, SRS::Response $response )
 {
-	$self->responses->{$id} = $response;
+	my $rq_a = $self->queue->[0];
+	my $rs_a = $self->responses->[0];
+	for ( my $i = 0; $i <= $#$rq_a; $i++ ) {
+		if ( $rq_a->[$i] == $request ) {
+			$rs_a->[$i] = $response;
+		}
+	}
 }
 
 method backend_response_ready() {
-	my $id = $self->queue->[0]->action_id;
-	exists $self->responses->{$id};
+	my $rq_a = $self->queue->[0]
+		or return;
+	my $rs_a = $self->responses->[0];
+	@$rq_a == @$rs_a;
 }
 
 method dequeue_backend_response() {
 	if ( $self->backend_response_ready ) {
-		my $rq = shift @{ $self->queue };
+		my $rq_a = shift @{ $self->queue };
 		my $owner = shift @{ $self->owner };
+		my $rs_a = shift @{ $self->responses };
 		my $sent = $self->sent;
-		$sent--;
+		$sent -= scalar @$rq_a;
 		if ( $sent < 0 ) {
 			warn "Bug: sent < 0 ?";
 			$sent = 0;
 		}
 		$self->sent($sent);
-		my $id = $rq->action_id;
-		my $response = delete $self->responses->{$id};
+
 		if ( wantarray ) {
-			($owner, $response, $rq);
+			($owner, @$rs_a);
 		}
 		else {
-			$owner->notify($response);
-			$response;
+			$owner->notify(@$rs_a);
 		}
 	}
 	else {
@@ -102,3 +119,63 @@ method dequeue_backend_response() {
 1;
 
 __END__
+
+=head1 NAME
+
+SRS::EPP::Session::BackendQ - manage tx queue for back-end processing
+
+=head1 SYNOPSIS
+
+ my $q = SRS::EPP::Session::BackendQ->new( session => $session );
+
+ # put requests on queue
+ $q->queue_backend_request( $epp_command, @srs_requests );
+
+ # pull up to 6 requests off queue for processing
+ my @rq = $q->backend_next( 6 );
+
+ # put responses in, one by one.
+ for (1..6) {
+     $q->add_backend_response( $rq[$i], $rs[$i] );
+ }
+
+ # if a message has had all its requests answered, it can be dequeued
+ ($epp_command, @srs_responses)
+      = $q->dequeue_backend_response();
+
+=head1 DESCRIPTION
+
+This class implements a simple FIFO queue, but with small
+customizations to operation to suit the use case of the SRS EPP Proxy.
+
+=head1 SEE ALSO
+
+L<SRS::EPP::Session>
+
+=head1 AUTHOR AND LICENCE
+
+Development commissioned by NZ Registry Services, and carried out by
+Catalyst IT - L<http://www.catalyst.net.nz/>
+
+Copyright 2009, 2010, NZ Registry Services.  This module is licensed
+under the Artistic License v2.0, which permits relicensing under other
+Free Software licenses.
+
+=cut
+
+
+# Local Variables:
+# mode:cperl
+# indent-tabs-mode: t
+# cperl-continued-statement-offset: 8
+# cperl-brace-offset: 0
+# cperl-close-paren-offset: 0
+# cperl-continued-brace-offset: 0
+# cperl-continued-statement-offset: 8
+# cperl-extra-newline-before-brace: nil
+# cperl-indent-level: 8
+# cperl-indent-parens-as-block: t
+# cperl-indent-wrt-brace: nil
+# cperl-label-offset: -8
+# cperl-merge-trailing-else: t
+# End:
