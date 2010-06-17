d6b41f8f960c1b482c1553b7671a11b5ec17b2d7
Andrew Chilton <andy@catalyst.net.nz>
2010-06-17 15:00:20 +1200

    Complete Check Contacts response and tests

--- lib/SRS/EPP/Command/Check/Contact.pm
+++ lib/SRS/EPP/Command/Check/Contact.pm
@@ -22,13 +22,46 @@ method to_srs() {
     return XML::SRS::Handle::Query->new( handle_id_filter => $payload->ids );
 }
 
+has 'ids_to_check' =>
+    is => 'rw',
+    isa => 'ArrayRef[Str]',
+    ;
+
 has 'avail' =>
     is => "rw",
-    isa => "ArrayRef[Str]",
+    isa => "HashRef[Str]",
     ;
 
 method notify( SRS::EPP::SRSResponse @rs ) {
-    $self->avail([ map { $_->message->ActionResponse->status } @rs ]);
+    $self->avail({ map { $_->message->response->handle_id => 1 } grep { $_->message->response } @rs });
 };
+
+method response() {
+    my $epp = $self->message;
+    my $payload = $epp->message->argument->payload;
+
+    my $ids = $payload->ids;
+    my $avail = $self->avail();
+
+    my @ids = map { XML::EPP::Contact::Check::ID->new(
+                        name => $_,
+                        available => ($avail->{$_} ? 0 : 1),
+                        ) } @$ids;
+
+    my $status = XML::EPP::Contact::Check::Status->new(
+        id_status => \@ids,
+    );
+
+    my $r = XML::EPP::Contact::Check::Response->new(
+        items => [ $status ],
+    );
+
+    # from SRS::EPP::Response::Check
+    return $self->make_response(
+        'Check',
+        code => 1000,
+        payload => $r,
+        );
+}
 
 1;
