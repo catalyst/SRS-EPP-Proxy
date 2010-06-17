d3f68126d6b2e1a30401b9004d2d277cbbd2459e
Evan Giles <evan@catalyst.net.nz>
2010-06-17 15:23:31 +1200

    Add Delete Contact command

---
+++ lib/SRS/EPP/Command/Delete/Contact.pm
@@ -0,0 +1,61 @@
+
+
+package SRS::EPP::Command::Delete::Contact;
+
+use Moose;
+extends 'SRS::EPP::Command::Delete';
+use MooseX::Method::Signatures;
+use Crypt::Password;
+use SRS::EPP::Session;
+use XML::EPP::Contact;
+
+# for plugin system to connect
+sub xmlns {
+    XML::EPP::Contact::Node::xmlns();
+}
+
+method to_srs() {
+    my $epp = $self->message;
+    my $message = $epp->message;
+
+    my $payload = $message->argument->payload;
+    my $action_id = $message->client_id || sprintf("auto.%x",time());
+
+    return XML::SRS::Handle::Update->new(
+            handle_id => $payload->id,
+            action_id => $action_id,
+            delete => 1,
+            );
+}
+
+has 'evan' => (
+    is => "rw",
+    isa => "Int",
+    lazy => 1,
+    default => 2400,   # Command failed
+);
+
+method notify( SRS::EPP::SRSResponse @rs ) {
+  my $message = $rs[0]->message;
+  my $response = $message->response;
+
+  if ( ! $response ) {
+    # That means everything worked
+    return $self->evan(1000);
+  }
+
+  if ( $response->isa("XML::SRS::Error") ) {
+    if ( $response->ErrorId() eq "HANDLE_DOES_NOT_EXIST" ) {
+      return $self->evan(2303);
+    }
+  }
+};
+
+method response() {
+    my $epp = $self->message;
+    my $payload = $epp->message->argument->payload;
+
+    $self->make_response(code => $self->evan());
+}
+
+1;
