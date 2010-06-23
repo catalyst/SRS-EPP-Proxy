750a431f7d793e23e55b4b3247d2a1aa4a09f05e
Andrew Chilton <andy@catalyst.net.nz>
2010-06-23 14:16:32 +1200

    process() now done for Info/Domain->DomainDetailsQry

---
+++ lib/SRS/EPP/Command/Info/Domain.pm
@@ -0,0 +1,29 @@
+package SRS::EPP::Command::Info::Domain;
+
+use Moose;
+extends 'SRS::EPP::Command::Info';
+use MooseX::Method::Signatures;
+use Crypt::Password;
+use SRS::EPP::Session;
+use XML::EPP::Domain;
+use Data::Dumper;
+
+# for plugin system to connect
+sub xmlns {
+    XML::EPP::Domain::Node::xmlns();
+}
+
+method process( SRS::EPP::Session $session ) {
+    $self->session($session);
+    my $epp = $self->message;
+    my $payload = $epp->message->argument->payload;
+
+    # we're not supporting authInfo, so get out of here with an EPP response
+    if ( $payload->has_auth_info ) {
+        return $self->make_response(code => 2307);
+    }
+
+    return XML::SRS::Domain::Query->new( domain_name_filter => $payload->name->value );
+}
+
+1;
