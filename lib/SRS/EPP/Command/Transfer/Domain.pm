ee5405198a5a8e2b6dc9c9b5ad9c488df3413ae0
Evan Giles <evan@catalyst.net.nz>
2010-07-02 11:07:58 +1200

    Add a test for an invalid UDAI (previously caused a crash)

--- lib/SRS/EPP/Command/Transfer/Domain.pm
+++ lib/SRS/EPP/Command/Transfer/Domain.pm
@@ -6,10 +6,12 @@ use Moose;
 extends 'SRS::EPP::Command::Transfer';
 
 use MooseX::Method::Signatures;
+use Moose::Util::TypeConstraints();
 use Crypt::Password;
 use SRS::EPP::Session;
 use XML::EPP::Domain;
 use XML::SRS::TimeStamp;
+use XML::SRS::Types;
 
 # for plugin system to connect
 sub xmlns {
@@ -29,6 +31,11 @@ method process( SRS::EPP::Session $session ) {
     # Get the auth info (could do some more validation here...)
     my $auth = $payload->auth_info();
     my $pass = $auth->pw();
+    my $udaiType = Moose::Util::TypeConstraints::find_type_constraint("XML::SRS::UDAI");
+    if ( ! $udaiType->check($pass->content()) ) {
+      return $self->make_response(code => 2202);
+    }
+
     return (
       XML::SRS::Whois->new(
         domain => $payload->name,
