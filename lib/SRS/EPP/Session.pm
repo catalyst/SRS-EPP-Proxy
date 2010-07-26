catalyst/SRS-EPP-Proxy

commit 5f4149732affb47210f6708849b8c24534849a1f

Revert "Session: unify mapping response code"
This reverts commit 0bf3566d6a0a03cb29d59e832ff985a3493a3d97.

The change doesn't seem to work... the problem is, it requires
proces_queue to be called multiple times for a single request,
which means the queue loses track of it's counter. It ends up
looking at a request that's not there, and the server hangs.

This will obviously need to be re-thought at a later date, but
reverting for now to allow testing to progress

lib/SRS/EPP/Session.pm
@@ -347,8 +347,63 @@ method process_queue( Int $count = 1 ) {
 		}
 		else {
 			# regular message which may need to talk to the SRS backend
-			my @messages = eval { $command->process($self) };
-			$self->process_result($command, @messages);
+			my @messages = $command->process($self);
+
+			# check what kind of messages these are
+			if ( $messages[0] and
+			     $messages[0]->does('XML::SRS::Action') ||
+			     $messages[0]->does('XML::SRS::Query') ) {
+				@messages = map {
+					SRS::EPP::SRSRequest->new(
+						message => $_,
+						);
+					} @messages;
+				$self->log_info(
+				"command produced ".@messages." SRS messages"
+					);
+				$self->queue_backend_request(
+					$command, @messages,
+					);
+				if ( $command->isa("SRS::EPP::Command::Login") ) {
+					$self->state("Processing <login>");
+					#$self->stalled(1);
+				}
+				else {
+					$self->state("Processing Command");
+				}
+				$self->yield("send_backend_queue");
+			}
+			elsif ( $messages[0] and
+			        $messages[0]->isa('XML::EPP') ) {
+				# add these messages to the outgoing queue
+				die "wrong" if @messages > 1;
+				my $response = SRS::EPP::EPPResponse->new(
+					message => $messages[0],
+					);
+
+				# add to the queue
+				$self->add_command_response(
+					$response, $command,
+					);
+			}
+			else {
+				# something else, or default response.
+				my $rs;
+
+				if ( defined $messages[0] and
+				     $messages[0] =~ /^\d{4}$/ ) {
+					# error code.
+					my $rs = $command->make_response(
+						@messages,
+						);
+					$self->add_command_response(
+						SRS::EPP::EPPResponse->new(
+							message => $rs,
+							),
+						$command,
+						);
+				}
+			}
 		}
 		$self->yield("send_pending_replies")
 			if $self->response_ready;
@@ -577,78 +632,35 @@ method be_response( SRS::EPP::SRSMessage $rs_tx ) {
 	$self->yield("process_responses");
 }
 
-method process_result( SRS::EPP::Command $command, @messages) {
-
-	# check what kind of messages these are
-	if ( $messages[0] and
-	     $messages[0]->does('XML::SRS::Action') ||
-	     $messages[0]->does('XML::SRS::Query') ) {
-		@messages = map {
-			SRS::EPP::SRSRequest->new(
-				message => $_,
-				);
-		} @messages;
-		$self->log_info(
-			"command produced ".@messages." SRS messages"
-			);
-		$self->queue_backend_request(
-			$command, @messages,
-			);
-		if ( $command->isa("SRS::EPP::Command::Login") ) {
-			$self->state("Processing <login>");
-			#$self->stalled(1);
-		}
-		else {
-			$self->state("Processing Command");
-		}
-		$self->yield("send_backend_queue");
-	}
-	elsif ( $messages[0] and
-			$messages[0]->isa('XML::EPP') ) {
-		# add these messages to the outgoing queue
-		die "wrong" if @messages > 1;
-		my $response = SRS::EPP::Response->new(
-			message => $messages[0],
-			);
-
-		# add to the queue
-		$self->add_command_response(
-			$response, $command,
-			);
-	}
-	elsif ( $messages[0] and
-			$messages[0]->isa('SRS::EPP::Response') ) {
-		die "wrong" if @messages > 1;
-		# fully cooked response
-		$self->add_command_response(
-			$messages[0], $command,
-			);
-	}
-	else {
-		# something else / error
-		my $rs;
-		if ( defined $messages[0] and
-			     $messages[0] =~ /^\d{4}$/ ) {
-			# error code.
-			my $rs = $command->make_response(
-				@messages,
-				);
-			$self->add_command_response(
-				SRS::EPP::EPPResponse->new(
-					message => $rs,
-					),
-				$command,
-				);
-		}
-	}
-}
-
 method process_responses() {
 	while ( $self->backend_response_ready ) {
 		my ($cmd, @rs) = $self->dequeue_backend_response;
 		$self->log_info("notifying command $cmd of back-end response");
-		my @messages = eval { $cmd->notify(@rs) };
-		$self->process_result($cmd, @messages);
+		my $resp = $cmd->notify(@rs);
+		if ( $resp->isa("SRS::EPP::Response") ) {
+			$self->log_info( "command $cmd is complete" );
+			$self->state("Prepare Response");
+			$self->log_debug(
+				"response to $cmd is response $resp",
+				);
+			$self->add_command_response($resp, $cmd);
+			$self->yield("send_pending_replies")
+				if $self->response_ready;
+		}
+		elsif ( $resp->isa("XML::SRS") ) {
+			$self->log_info( "command $cmd not yet complete" );
+			my @messages = map {
+				SRS::EPP::SRSRequest->new(
+					message => $_,
+					);
+			} $resp;
+			$self->log_info(
+				"command $cmd produced ".@messages
+					." further SRS messages"
+				);
+			$self->queue_backend_request($cmd, @messages);
+			$self->yield("send_backend_queue");
+		}
 	}
 }
 

commit ea7b173046c1de4d4ac88ec6cc65cb0400832c6f

Fixed bug in handling of responses
This was probably introduced by 5f4149732affb47210f6708849b8c24534849a1f,
which reverted to a much earlier version. process_responses() wasn't
handling SRS responses correctly.

lib/SRS/EPP/Session.pm
@@ -657,6 +657,7 @@ method process_responses() {
 		my ($cmd, @rs) = $self->dequeue_backend_response;
 		$self->log_info("notifying command $cmd of back-end response");
 		my $resp = $cmd->notify(@rs);
+		$self->log_trace("Resp isa " . ref $resp);
 		if ( $resp->isa("SRS::EPP::Response") ) {
 			$self->log_info( "command $cmd is complete" );
 			$self->state("Prepare Response");
@@ -667,7 +668,8 @@ method process_responses() {
 			$self->yield("send_pending_replies")
 				if $self->response_ready;
 		}
-		elsif ( $resp->isa("XML::SRS") ) {
+        elsif ( $resp->does('XML::SRS::Action') ||
+                $resp->does('XML::SRS::Query') ) {
 			$self->log_info( "command $cmd not yet complete" );
 			my @messages = map {
 				SRS::EPP::SRSRequest->new(
@@ -680,6 +682,10 @@ method process_responses() {
 				);
 			$self->queue_backend_request($cmd, @messages);
 			$self->yield("send_backend_queue");
+		}
+		else {
+		    # TODO: should handle this more gracefully, i.e. return a response to the client
+            confess "Unknown response type: " . ref $resp;    
 		}
 	}
 }