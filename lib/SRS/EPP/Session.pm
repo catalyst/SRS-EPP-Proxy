17e7caa57b90742c7b4e933f5c34bbb0e5f6a6f6
Sam Crawley <samc@catalyst.net.nz>
2010-07-28 13:56:11 +1200

    Need to be a bit more careful about preserving the contents of $@

--- lib/SRS/EPP/Session.pm
+++ lib/SRS/EPP/Session.pm
@@ -637,7 +637,8 @@ method backend_response() {
 	# decode message
 	$self->log_packet("BE response", $fields{r});
 	my $message = eval { XML::SRS::Response->parse($fields{r}) };
-	confess "Exception parsing SRS Response: $@" if $@;
+	my $error = $@;
+	confess "Exception parsing SRS Response: $error" if $error;
 	my $rs_tx = SRS::EPP::SRSMessage->new( message => $message );
 
 	$self->be_response($rs_tx);
