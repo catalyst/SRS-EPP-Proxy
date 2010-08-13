2758287352eabf24c9781788dccfbdadcd767f2a
Sam Crawley <samc@catalyst.net.nz>
2010-08-13 14:36:25 +1200

    wr72389: reduced i/o timeouts

--- lib/SRS/EPP/Session.pm
+++ lib/SRS/EPP/Session.pm
@@ -469,7 +469,7 @@ method connected() {
 			$self->log_trace("got input callback");
 			$self->input_event;
 		},
-		timeout => 120,
+		timeout => 30,
 		timeout_cb => sub {
 			$self->log_trace("got input timeout event");
 			$self->input_timeout;
@@ -484,7 +484,7 @@ method connected() {
 		cb => sub {
 			$self->output_event;
 		},
-		timeout => 120,
+		timeout => 30,
 		timeout_cb => sub {
 			$self->log_trace("got output timeout event");
 		},
