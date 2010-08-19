984e420c251c4f93c8fa1d66498b3c08e8738fb9
Sam Crawley <samc@catalyst.net.nz>
2010-08-19 16:23:50 +1200

    wr72907: increase io event timeouts
    After removing the segfault issue, suddenly things are randomly aborting.
    This seems to fix it.

--- lib/SRS/EPP/Session.pm
+++ lib/SRS/EPP/Session.pm
@@ -466,7 +466,7 @@ method connected() {
 			$self->log_trace("got input callback");
 			$self->input_event;
 		},
-		timeout => 30,
+		timeout => 300,
 		timeout_cb => sub {
 			$self->log_trace("got input timeout event");
 			$self->input_timeout;
@@ -481,7 +481,7 @@ method connected() {
 		cb => sub {
 			$self->output_event;
 		},
-		timeout => 30,
+		timeout => 300,
 		timeout_cb => sub {
 			$self->log_trace("got output timeout event");
 		},
