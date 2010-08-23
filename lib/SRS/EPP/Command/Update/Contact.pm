cb74e097e589d7b4d4b05770753d59ad278ed7eb
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-08-23 20:44:19 +0100

    wr69417 - [ws] <contact:update> and Common tidy-up
    Also make Common::Contact use MooseX::Method::Signatures so we can see
    what its argument types are without a code survey.

--- lib/SRS/EPP/Command/Update/Contact.pm
+++ lib/SRS/EPP/Command/Update/Contact.pm
@@ -11,66 +11,65 @@ use MooseX::Method::Signatures;
 
 # for plugin system to connect
 sub xmlns {
-    return XML::EPP::Contact::Node::xmlns();
+	return XML::EPP::Contact::Node::xmlns();
 }
 
 method process( SRS::EPP::Session $session ) {
-    $self->session($session);
+	$self->session($session);
 
-    my $epp = $self->message;
-    my $message = $epp->message;
-    my $payload = $message->argument->payload;
+	my $epp = $self->message;
+	my $message = $epp->message;
+	my $payload = $message->argument->payload;
 
-    # Reject add or remove elements, since those are just statuses which we don't support
-    if ( $payload->add || $payload->remove) {
-        return $self->make_response(code => 2307);
-    }
+	# Reject add or remove elements, since those are just statuses
+	# which we don't support
+	if ( $payload->add || $payload->remove) {
+		return $self->make_response(code => 2307);
+	}
 
-    # Must supply a change element
-    unless ( $payload->change ) {
-        return $self->make_response(code => 2002);
-    }
+	# Must supply a change element
+	unless ( $payload->change ) {
+		return $self->make_response(code => 2002);
+	}
 
-    my $contact = $payload->change;
+	my $contact = $payload->change;
 
-    # Check they haven't given us some invalid fields
-    if (my $resp = $self->validate_epp_contact($contact)) {
-        return $resp;
-    }
+	# Check they haven't given us some invalid fields
+	if (my $resp = $self->validate_epp_contact($contact)) {
+		return $resp;
+	}
 
-    my $address;
-    my $name;
-    if ($contact->postal_info) {
-        $address = $self->translate_address($contact->postal_info->[0]->addr);
+	my $address;
+	my $name;
+	if ($contact->postal_info) {
+		$address = $self->translate_address(
+			$contact->postal_info->[0]->addr,
+		       );
 
-        # Blank out any optional fields they didn't provide in the address. Otherwise
-        #  the original values will be left in by the SRS (EPP considers the
-        #  address one unit to be replaced)
-        for my $field (qw/address2 region postcode/) {
-            $address->$field('') unless $address->$field;
-        }
-        
-        $name = $contact->postal_info->[0]->name;
-    }
+		# Blank out any optional fields they didn't provide in
+		# the address. Otherwise the original values will be
+		# left in by the SRS (EPP considers the address one
+		# unit to be replaced)
+		for my $field (qw/address2 region postcode/) {
+			$address->$field('') unless $address->$field;
+		}
 
+		$name = $contact->postal_info->[0]->name;
+	}
 
-    return (
-        XML::SRS::Handle::Update->new(
-            handle_id => $payload->id,
-            action_id => $message->client_id || $self->server_id,
-            ($name ? (name => $name) : ()),
-            ($address ? (address => $address) : ()),
-            ($contact->voice ? (phone => $contact->voice->content) : ()),
-            ($contact->fax ? (fax => $contact->fax->content) : ()),
-            ($contact->email ? (email => $contact->email) : ()),
-        )
-    );
+	return XML::SRS::Handle::Update->new(
+		handle_id => $payload->id,
+		action_id => $message->client_id || $self->server_id,
+		($name ? (name => $name) : ()),
+		($address ? (address => $address) : ()),
+		($contact->voice ? (phone => $contact->voice->content) : ()),
+		($contact->fax ? (fax => $contact->fax->content) : ()),
+		($contact->email ? (email => $contact->email) : ()),
+	       );
 }
 
 method notify( SRS::EPP::SRSResponse @rs ) {
-    return $self->make_response(code => 1000);
+	return $self->make_response(code => 1000);
 }
-
-
 
 1;
--- lib/SRS/EPP/Common/Contact.pm
+++ lib/SRS/EPP/Common/Contact.pm
@@ -1,84 +1,81 @@
 package SRS::EPP::Common::Contact;
 
 use Moose::Role;
+use MooseX::Method::Signatures;
+use Moose::Util::TypeConstraints;
 
-requires 'make_response';
+requires 'make_error';
+
+BEGIN {
+	subtype 'SRS::EPP::Common::Contact::Arg'
+		=> as "XML::EPP::Contact::Change|XML::EPP::Contact::Create";
+}
 
 # Check if an epp contact has certain field we don't support
 #  Return an error message if they do, nothing if it's valid
-sub validate_epp_contact {
-    my $self    = shift;
-    my $contact = shift;
+method validate_epp_contact( SRS::EPP::Common::Contact::Arg $contact ) {
 
-    my $epp_postal_info = $contact->postal_info();
-    if ( $epp_postal_info && scalar @$epp_postal_info != 1 ) {
+	my $epp_postal_info = $contact->postal_info();
+	if ( $epp_postal_info && scalar @$epp_postal_info != 1 ) {
 
-        # The SRS doesn't support the US's idea of i18n.  That is
-        # that ASCII=international, anything else=local.
-        # Instead, well accept either form of postalinfo, but throw an
-        # error if they try to provide both types (because the SRS can't
-        # have two translations for one address)
-        return $self->make_response(
-            Error => (
-                code      => 2306,
-                exception => XML::EPP::Error->new(
-                    value  => '',
-                    reason => 'Only one postal info element per contact supported',
-                ),
-            )
-        );
-    }
-    my $postalInfo = $epp_postal_info->[0];
+		# The SRS doesn't support the US's idea of i18n.  That
+		# is that ASCII=international, anything else=local.
+		# Instead, well accept either form of postalinfo, but
+		# throw an error if they try to provide both types
+		# (because the SRS can't have two translations for one
+		# address)
 
-    return unless $postalInfo;
+		return $self->make_error(
+			code => 2306,
+			value  => '',
+			reason =>
+		'Only one postal info element per contact supported',
+		       );
+	}
+	my $postalInfo = $epp_postal_info->[0];
 
-    # The SRS doesn't have a 'org' field, we don't want to lose info, so
-    if ( $postalInfo->org ) {
-        return $self->make_response(
-            Error => (
-                code      => 2306,
-                exception => XML::EPP::Error->new(
-                    value  => $postalInfo->org,
-                    reason => 'org field not supported',
-                ),
-            )
-        );
-    }
+	return unless $postalInfo;
 
-    # SRS requires at least one address line, but not more than 2. Reject request if they send 0
-    #  or 3 street lines
-    my $street_lines = $postalInfo->addr->street;
-    if (! $street_lines || scalar @$street_lines < 1 || @$street_lines > 2) {
-        return $self->make_response(
-            Error => (
-                code      => 2306,
-                exception => XML::EPP::Error->new(
-                    value  => '',
-                    reason => 'At least 1 and no more than 2 street lines must be supplied in the address',
-                ),
-            )
-        );
-    }
+	# The SRS doesn't have a 'org' field, we don't want to lose
+	# info, so
+	if ( $postalInfo->org ) {
+		return $self->make_error(
+			code => 2306,
+			value  => $postalInfo->org,
+			reason => 'org field not supported',
+		       );
+	}
 
-    return;
+	# SRS requires at least one address line, but not more than
+	# 2; Reject request if they send 0 or 3 street lines
+	my $street_lines = $postalInfo->addr->street;
+	if (! $street_lines || scalar @$street_lines < 1
+		    || @$street_lines > 2) {
+		return $self->make_error(
+			code => 2306,
+			value  => '',
+			reason =>
+'At least 1 and no more than 2 street lines must be supplied in the address',
+		       );
+	}
+
+	return;
 }
 
 # Turn an epp address into an srs address
-sub translate_address {
-    my $self = shift;
-    my $epp_address = shift;
+method translate_address( XML::EPP::Contact::Addr $epp_address ) {
 
-    my $street  = $epp_address->street();
-    my $address = XML::SRS::Contact::Address->new(
-        address1 => $street->[0],
-        ( $street->[1] ? (address2 => $street->[1]) : () ),
-        city     => $epp_address->city,
-        ( $epp_address->sp ? ( region => $epp_address->sp ) : () ),
-        cc => $epp_address->cc,
-        ( $epp_address->pc ? ( postcode => $epp_address->pc ) : () ),
-    );
+	my $street = $epp_address->street();
+	my $address = XML::SRS::Contact::Address->new(
+		address1 => $street->[0],
+		( $street->[1] ? (address2 => $street->[1]) : () ),
+		city => $epp_address->city,
+		( $epp_address->sp ? ( region => $epp_address->sp ) : () ),
+		cc => $epp_address->cc,
+		( $epp_address->pc ? ( postcode => $epp_address->pc ) : () ),
+	       );
 
-    return $address;
+	return $address;
 }
 
 1;
