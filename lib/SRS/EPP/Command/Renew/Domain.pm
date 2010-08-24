c55ed53eb91ecc03bea8c55c1f5ed8bae16b90ca
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-08-25 00:25:56 +0100

    wr73095 - rework logic around expiry dates
    Also some structural simplification.  If you are using if() { } statements
    that test preconditions, and the using "return" in the middle, there really
    is little point in avoiding goto.  goto might be if's evil twin, but they
    are still closely related.
    
    This introduces a new error for the caught condition that the current
    expiry date passed in is wrong.

--- lib/SRS/EPP/Command/Renew/Domain.pm
+++ lib/SRS/EPP/Command/Renew/Domain.pm
@@ -1,5 +1,4 @@
 
-
 package SRS::EPP::Command::Renew::Domain;
 
 use Moose;
@@ -13,86 +12,97 @@ use XML::SRS::TimeStamp;
 
 # for plugin system to connect
 sub xmlns {
-    XML::EPP::Domain::Node::xmlns();
+	XML::EPP::Domain::Node::xmlns();
 }
 
-method duplicateRenew ( XML::SRS::TimeStamp $domainDate, Str $txnDate ) {
-  # This is a bit mad!
-  # The idea is that we just want to ensure that the same transaction doesn't
-  # get repeated, and renew a domain twice.
-  #
-  # We are going to be very forgiving here - if the
-  # String provied as part of the EPP transaction is within a few days of the
-  # billed_until date on the domain - we'll allow the renew to happen
+use MooseX::TimestampTZ qw(epoch);
 
-  my $domEpoch = $domainDate->epoch();
-  my $txnEpoch = XML::SRS::TimeStamp->new(timestamp => "$txnDate 00:00:00")->epoch();
+method dates_approx_match ( XML::SRS::TimeStamp $domain_date, Str $txn_date ) {
 
-  my $diff = $domEpoch - $txnEpoch;
-  return abs($diff) > (86400*2);
+	my $domain_epoch = $domain_date->epoch();
+	my $txn_epoch = epoch "$txn_date 00:00:00Z";
+
+	my $diff = $domain_epoch - $txn_epoch;
+	return abs($diff) < (86400*2);
 }
 
 method process( SRS::EPP::Session $session ) {
-  $self->session($session);
+	$self->session($session);
 
-  my $epp = $self->message;
-  my $message = $epp->message;
-  my $payload = $message->argument->payload;
+	my $epp = $self->message;
+	my $message = $epp->message;
+	my $payload = $message->argument->payload;
 
-  return XML::SRS::Whois->new(
-    domain => $payload->name(),
-    );
+	$session->stalled($self);
+
+	return XML::SRS::Whois->new(
+		domain => $payload->name(),
+		);
 }
 
 has 'billed_until' =>
-    is => "rw",
-    isa => "XML::SRS::TimeStamp",
-    ;
+	is => "rw",
+	isa => "XML::SRS::TimeStamp",
+	;
 
 method notify( SRS::EPP::SRSResponse @rs ) {
-  my $epp = $self->message;
-  my $eppMessage = $epp->message;
-  my $eppPayload = $eppMessage->argument->payload;
+	my $epp = $self->message;
+	my $eppMessage = $epp->message;
+	my $eppPayload = $eppMessage->argument->payload;
 
-  my $message = $rs[0]->message;
-  my $response = $message->response;
+	my $message = $rs[0]->message;
+	my $response = $message->response or goto error_out;
 
-  if ( ! $self->billed_until() ) {
-    # This must be a response to our query TXN
+	if ( ! $self->billed_until() ) {
+		# This must be a response to our query TXN
 
-    if ( $response ) {
-      if ( $response->status eq "Available" ) {
-        return $self->make_response(code => 2303);
-      }
-      if ( my $billDate = $response->billed_until() ) {
-          if ( ! $self->duplicateRenew($billDate,$eppPayload->expiry_date) ) {
-            $self->billed_until($response->billed_until());
-            return XML::SRS::Domain::Update->new(
-              filter => [$response->name],
-              action_id => $self->client_id || $self->server_id,
-              renew => 1,
-              term => $eppPayload->period->months,
-              );
-          }
-      }
-    }
-    return $self->make_response(code => 2400);
-  }
+		if ( $response->status eq "Available" ) {
+			return $self->make_response(
+				code => 2303,
+				);
+		}
+		my $billed_until = $response->billed_until
+			or goto error_out;
 
-  # By now, we must be dealing with the response to our update TXN
+		if ( ! $self->dates_approx_match(
+			$billed_until,
+			$eppPayload->expiry_date,
+		       ) ) {
+			my $current = $billed_until->date;
+			my $reason = "Not close enough to current "
+				."expiry date ($current)";
+			return $self->make_error(
+				code => 2304,
+				value => $eppPayload->expiry_date,
+				reason => $reason,
+				);
+		}
 
-  if ( $response->can("billed_until") ) {
-    my $epp_resp = XML::EPP::Domain::Renew::Response->new(
-        name => $response->name,
-        expiry_date => $response->billed_until->timestamptz,
-    );
-    return $self->make_response(
-        code => 1000,
-        payload => $epp_resp,
-    );
-  }
+		$self->billed_until( $response->billed_until() );
+		$self->session->stalled(0);
 
-  return $self->make_response(code => 2400);
+		return XML::SRS::Domain::Update->new(
+			filter => [$response->name],
+			action_id => $self->client_id || $self->server_id,
+			renew => 1,
+			term => $eppPayload->period->months,
+			);
+	}
+
+	# By now, we must be dealing with the response to our update TXN
+	if ( $response->can("billed_until") ) {
+		my $epp_resp = XML::EPP::Domain::Renew::Response->new(
+			name => $response->name,
+			expiry_date => $response->billed_until->timestamptz,
+			);
+		return $self->make_response(
+			code => 1000,
+			payload => $epp_resp,
+			);
+	}
+
+error_out:
+	return $self->make_response(code => 2400);
 }
 
 1;
