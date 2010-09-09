e0fde0c31c56fb11094bdf859cdd6fc230fe3d81
Sam Crawley <samc@catalyst.net.nz>
2010-09-10 08:51:09 +1200

    wr73578: fixed error message when a delete domain is attempted on a domain the registrar doesn't own

--- lib/SRS/EPP/Command/Delete/Domain.pm
+++ lib/SRS/EPP/Command/Delete/Domain.pm
@@ -46,4 +46,23 @@ method notify( SRS::EPP::SRSResponse @rs ) {
 	return $self->make_response(code => 2400);
 }
 
+method make_error_response( ArrayRef[XML::SRS::Error] $srs_errors ) {
+
+	# If we get an error about a missing UDAI, then this must be a
+	#   domain the registrar doesn't own. Return an appropriate
+	#   epp error
+	foreach my $srs_error (@$srs_errors) {
+		if ($srs_error->error_id eq 'MISSING_MANDATORY_FIELD') {
+			if ($srs_error->details && $srs_error->details->[0] eq 'UDAI') {
+				return $self->make_error(
+					code    => 2201,
+					message => 'Authorization Error',
+				);
+			}
+		}
+	}
+
+	return $self->SUPER::make_error_response($srs_errors);
+}
+
 1;
